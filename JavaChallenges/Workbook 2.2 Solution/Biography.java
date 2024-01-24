21a2f91775c0be404e31962f0e2932f1b39fd584
Aashish Kumar Jha <20bcs3872@cuchd.in>
2024-01-24 11:04:29 +0530

    added workbook 2.2 solution

---
+++ JavaChallenges/Workbook 2.2 Solution/Biography.java
@@ -0,0 +1,12 @@
+public class Biography {
+    public static void main(String[] args) {
+        String name = "Aashish", country = "India", sport = "Volleyball", game = "BGMI", subject = "Maths";
+        char grade = 'A';
+        int age = 22, hours = 2;
+
+        System.out.println("My name is " + name + ". I'm " +  age + " years old, and I'm from " + country + ".");
+        System.out.println("My favorite sport is "+ sport + ". I play for "  + hours + " hour(s) a day");
+        System.out.println("When I'm tired, I like to play "+ game + ".");
+        System.out.println("In school, my favourite subject was "+subject+ ". I scored a " + grade + ".");
+    }
+}
