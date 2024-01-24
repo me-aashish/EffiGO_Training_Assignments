bb9b4773cf98c9555e11c59f51ef235b79c0a376
Aashish Kumar Jha <20bcs3872@cuchd.in>
2024-01-24 10:46:02 +0530

    added challenge 1

---
+++ JavaChallenges/Challenge 1/YourInitials.java
@@ -0,0 +1,11 @@
+public class YourInitials {
+    public static void main(String[] args) {
+        System.out.println("  A     A  ");
+        System.out.println(" A A   A A ");
+        System.out.println("A   A A   A");
+        System.out.println("AAAAA AAAAA");
+        System.out.println("A   A A   A");
+        System.out.println("A   A A   A");
+        System.out.println("A   A A   A");
+    }
+}
