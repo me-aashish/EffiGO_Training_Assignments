0cc045be84863e4cf3da93ebc9f3d7fe870cd2e7
Aashish Kumar Jha <20bcs3872@cuchd.in>
2024-01-29 16:53:28 +0530

    modified contacts.java

--- JavaChallenges/Extra Practice (OOPS)/Part 1/Contact.java
+++ JavaChallenges/Extra Practice (OOPS)/Part 1/Contact.java
@@ -4,19 +4,16 @@ public class Contact {
     private String birthDate;
     private int age;
 
-
-    public Contact(String name, String phoneNumber, String birthDate, int age) {
+    public Contact(String name, String phoneNumber, String birthDate) {
         this.name = name;
         this.phoneNumber = phoneNumber;
         this.birthDate = birthDate;
-        this.age = age;
     }
 
-    public Contact(Contact source){
+    public Contact(Contact source) {
         this.name = source.name;
         this.phoneNumber = source.phoneNumber;
         this.birthDate = source.birthDate;
-        this.age = source.age;
     }
-    
+
 }
